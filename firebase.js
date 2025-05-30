7d34d4358dd9e80f147452cde239a35ac9df0cf2
Nipa Sinthaisong <122784351+nipa0107@users.noreply.github.com>
2025-05-30 17:09:31 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domai",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "44887461779",
+  appId: "your-sender-id",
+  measurementId: "your-app-id"
 };
 
 
